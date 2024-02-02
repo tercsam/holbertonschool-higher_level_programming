08eb3b377f923740e23f7ada364c0e0586a9c06f
tercsam <mascret.clement@gmail.com>
2024-02-02 12:57:20 -0800

    update prog 7

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,10 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    for ligne in matrix:
-        for element in ligne:
-            print("{:d}".format(element), end=" ")
-            if element != ligne[-1]:
-                print(" ", end="")
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]), end='')
+            else:
+                print("{:d}".format(row[i]), end=" ")
         print()
