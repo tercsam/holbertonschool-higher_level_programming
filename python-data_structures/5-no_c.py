8d773802819239f6ce187368616cf9000c94cba6
tercsam <mascret.clement@gmail.com>
2024-02-02 12:56:49 -0800

    update prog 6

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    noCstring = str.maketrans(", ", "cC")
-    return my_string.translate(noCstring)
+
+    new_str = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            new_str += char
+    return new_str
