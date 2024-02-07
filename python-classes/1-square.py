64c6c9bb98051ad8a0eaf2856ab846d71002f4ef
tercsam <mascret.clement@gmail.com>
2024-02-06 23:26:49 -0800

    add prog n2

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"define a class with a private instance attribut 'size'"
+
+
+class Square:
+    "Square class with a private instance attribut"
+    def __init__(self, size):
+        "initalize square using pirvate attribute size as args"
+        self.size = size
