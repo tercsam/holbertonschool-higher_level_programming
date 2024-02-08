fd4f5204e39e379fbb5d8331f7b2e9319015c868
tercsam <mascret.clement@gmail.com>
2024-02-07 21:08:40 -0800

    update prog 4

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"define a class with a private instance attribut 'size'"
+"define empty class square for python."
 
 
-class Square:
-    "Square class with a private instance attribut"
-    def __init__(self, size):
+class Square():
+    "def private size attribute"
+    def __init__(self, size=0):
         "initalize square using pirvate attribute size as args"
         self.__size = size
 
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-def __init__(self, size=0):
-    if isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-
-def area(self):
-    return self.__size**2
+    def area(self):
+        return self.__size**2
 
     pass
