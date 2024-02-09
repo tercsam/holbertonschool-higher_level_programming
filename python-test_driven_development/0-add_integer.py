e1493e7406dd84e0fb11e2e7ce7b1507412d5363
tercsam <mascret.clement@gmail.com>
2024-02-09 10:05:17 -0800

    add dir + first prog + empty doctext file

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"define two intergers A and B"
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, int) or isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(a, float) or isinstance(b, int):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float) or isinstance(b, float):
+        
+        a == int
+        b == int
+    
+    return (a) + (b)
