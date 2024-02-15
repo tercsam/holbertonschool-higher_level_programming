33287ac71145d143fb4b25afce3eecf96a536b35
tercsam <mascret.clement@gmail.com>
2024-02-14 20:29:33 -0800

    add prog n2

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+need to add a comment
+
+"""
+
+
+class MyList(list):
+    """
+    need to add a comment
+    """
+    def print_sorted(self):
+        """
+        need to add a comment
+        """
+        print(sorted(self))
