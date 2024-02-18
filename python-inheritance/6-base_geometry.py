e89be5cdd5fa38b02b2d8fcb9099822d29d19db9
tercsam <mascret.clement@gmail.com>
2024-02-17 16:43:23 -0800

    update prog n6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 1-create an empty class
+    2-add a public instance method
+"""
+
+
+class BaseGeometry:
+    """ 1-empty class
+        2-raise an Exception:
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
