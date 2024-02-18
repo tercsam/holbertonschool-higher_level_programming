56840f9e46b6e7d8ae1403fefe867fd79c1c2e99
tercsam <mascret.clement@gmail.com>
2024-02-17 16:25:28 -0800

    add prog n5

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" need to add a comment """
+
+
+def inherits_from(obj, a_class):
+    """
+    need to add a comment
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
