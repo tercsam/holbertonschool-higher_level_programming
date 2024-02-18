63859a4a66a65fc543eeca76a0fdbe0640c70ad5
tercsam <mascret.clement@gmail.com>
2024-02-17 18:11:02 -0800

    add prog n9

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class rectangle"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
