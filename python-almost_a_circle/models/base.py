9becb136cbba74b3776d269e12ca303565b0e87a
tercsam <mascret.clement@gmail.com>
2024-02-19 10:27:29 -0800

    add all requirement and first prog

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""need a comment"""
+
+
+class Base:
+    """need comment"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialise.
+
+        Args:
+            id (int): an integer
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = + 1
+            self.id = Base.__nb_objects
